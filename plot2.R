634f1b1b083a8ef2c7aa16d93c90c674b278249f
fvon61 <fvon61@users.noreply.github.com>
2015-02-06 12:53:23 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -21,6 +21,6 @@ barplot(height=aggEmmByYear$Emissions, names.arg=aggEmmByYear$year,
         main=expression('Total PM'[2.5]*' in Baltimore City, MD'))
 
 ## Saving to file
-dev.copy(png, file="plot2a.png", height=480, width=480)
+dev.copy(png, file="plot2.png", height=480, width=480)
 
 dev.off()
